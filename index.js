0871ef2c0203ef62790b10938c23b3d41b059b93
AdrianWong <adrianwg04@gmail.com>
2023-05-05 21:04:11 +0100

    install updated inquirer module, added questions array,set up init(), create a generateMarkdown.js

--- index.js
+++ index.js
@@ -4,17 +4,73 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
+const questions = [{
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the project?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What is the description of the project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use the application?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use the application?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project:',
+        choices: [
+            'MIT',
+            'Apache',
+            'GPL',
+            'BSD'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How to contribute to the project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How to test your project?'
+    },
+    {
+        type: 'input',
+        name: 'gitHub_username',
+        message: 'What is your GitHub usrname?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,44 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [How to Contribute](#how to contribute)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} License
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## How to Contribute
+
+${data.contributing}
+
+## Tests
+
+${data.test}
+
 `;
 }
 
